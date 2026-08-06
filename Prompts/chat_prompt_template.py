9c056ed11df51280515c7aa8b833417d0c0932ce
deepak-patel-1999 <deepakpat9095@gmail.com>
2026-08-06 09:37:37 +0530

    message placeholder

--- Prompts/chat_prompt_template.py
+++ Prompts/chat_prompt_template.py
@@ -1,10 +1,11 @@
-# jese single dynmaic prompt bhejna ho to PromptTemplate use karte hai vese hi jab multiple dynamic prompt (list of prompt in a single list) bhejna hota hai tab ChatPromptTemplate ka use karte hai
+# jese single dynmaic prompt bhejna ho to PromptTemplate use karte hai vese hi jab multiple dynamic prompt (list of prompts in a single list) bhejna hota hai tab ChatPromptTemplate ka use karte hai
 
 from langchain_core.prompts import ChatPromptTemplate
 
+# jab ChatPromptTemplate ke through dynamic prompt generate krte hai tab hame AIMessage, SystemMessage, HumanMessage ka use nahi karte instead hum tuple ka use krte hai below is an example of that
 chat_template = ChatPromptTemplate([
-    ("system", "You are a helpful {domain} expert"),
-    ("human", "explain in simple terms what is {topic}"),
+    ("system", "You are a helpful {domain} expert"), # this is equal to SystemMessage class
+    ("human", "explain in simple terms what is {topic}"), # this is equal to HumanMessage class
 ])
 
 prompt = chat_template.invoke({"domain":"cricket", "topic":"dusra"})
---
+++ Prompts/message_placeholder.py
@@ -0,0 +1,19 @@
+# jab hume purane chats place krna ho ChatPromptTemplate ke andar tab hum MessagesPlaceholder class ka use karte hai ye ek list leta hai jisme user ki purani chats/messages pade hote hai. Automatically ye andar ke messages ko unpack kr dega
+
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+chat_template = ChatPromptTemplate([
+    ("system", "you are a customer support agent"),
+    MessagesPlaceholder(variable_name="chat_history"), # chat history should be a list of messages
+    ("human", "{query}")
+])
+
+chat_history = []
+
+with open("Prompts/chat_history.txt") as f:
+    chat_history.extend(f.readlines())
+
+prompt = chat_template.invoke({"chat_history": chat_history, "query": "Where is my refund"})
+
+
+print(prompt)
